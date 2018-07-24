a7b6e379ee3dc1a3687937f74fa3c0a3c9d31000
Rafael Soares <rafael.sores@neemu.com>
2018-07-24 13:40:23 -0400

    index example

--- index.rb
+++ index.rb
@@ -5,6 +5,7 @@ module AWSRuby
     require_relative 'lib/load_confs'
     require_relative 'lib/announcer'
     require_relative 'lib/appraiser'
+    require_relative 'lib/cluster_monitor'
 
     def self.setup
         throw RuntimeError("ENV credentials not set!") if ENV['AWS_ACCESS_KEY_ID'].nil? || ENV['AWS_SECRET_ACCESS_KEY'].nil?
@@ -20,21 +21,24 @@ module AWSRuby
     
         product_descriptions = [ "Linux/UNIX (Amazon VPC)" ]
 
-        ec2 = Aws::EC2::Client.new(region: region)
+        ec2_client = Aws::EC2::Client.new(region: region)
 
         # cost * (Estimation of 6h job / (day seconds * month))
         ebs_cost_per_gb = 0.1 * (21600.0 / (86400 * 30))
 
-        spot_client = SpotClient.new(ec2)
+        spot_client = SpotClient.new(ec2_client)
+
+        cluster_monitor = ClusterMonitor.new(ec2_client)
 
         { 
             spot_client: spot_client,
             region: region,
             product_descriptions: product_descriptions,
             ebs_cost: ebs_cost_per_gb,
-            confs: LoadConfs.cluster_confs
+            confs: LoadConfs.cluster_confs,
+            cluster_monitor: cluster_monitor
         }
-    end
+    end    
     
     def self.run
         args = setup        
@@ -43,14 +47,33 @@ module AWSRuby
         product_descriptions = args[:product_descriptions]
         confs = args[:confs]
         ebs_cost = args[:ebs_cost]
+        cluster_monitor = args[:cluster_monitor]
 
-        instances_history = spot_client.history_for(confs.map {|conf| conf.instance_type}, product_descriptions)
+        t = Time.now.utc
+
+        spot_history_filters = {
+            start_time: t,
+            end_time: t, 
+            instance_types: confs.map {|conf| conf.instance_type}, 
+            product_descriptions: product_descriptions
+        }
+
+        instances_history = spot_client.history_for(spot_history_filters)
 
         min_prices, best_conf = Appraiser.conf_costs(confs, ebs_cost, instances_history)
 
         Announcer.list_confs_min_prices(min_prices)
 
         Announcer.print_min_cost_conf(best_conf)
+
+        filters = [
+            { name: 'tag:Name', values: ['crm-elasticsearch5-prod'] },
+            { name: 'instance-state-name', values: ['running'] }
+        ]
+
+        cluster_info = cluster_monitor.cluster_info(filters)
+
+        Announcer.print_cluster_info(cluster_info)        
     end    
 
     run
